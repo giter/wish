76bbd169c849cb77c2ef602002c0a65d2380e120
wutongyu987 <wutongyu987@live.com>
2019-04-26 09:33:14 +0800

    wish

--- src/main/java/com/wish/control/OldMachineApi.java
+++ src/main/java/com/wish/control/OldMachineApi.java
@@ -1,6 +1,5 @@
 package com.wish.control;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.wish.bean.MachineGoodsBean;
